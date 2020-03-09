dd29bf7cc7aa28cd578975dec4d06924211e9eac
Tyler Churchill <tyjchurchill@gmail.com>
2020-03-08 18:39:17 -0700

    Basic template of Klondike created.

--- open_spiel/games/klondike.cc
+++ open_spiel/games/klondike.cc
@@ -1,14 +1,21 @@
 #include "klondike.h"
 
-#include <algorithm>
-#include <array>
+#include <deque>
 #include <numeric>
-#include <utility>
+#include <random>
+#include <algorithm>
+// #include <array>
+// #include <utility>
 
 #include "open_spiel/abseil-cpp/absl/strings/str_format.h"
 #include "open_spiel/abseil-cpp/absl/strings/str_join.h"
 #include "open_spiel/game_parameters.h"
 #include "open_spiel/spiel_utils.h"
+
+#define RED    "\033[31m"
+#define WHITE  "\033[37m"
+#define YELLOW "\033[33m"
+#define RESET  "\033[0m"
 
 namespace open_spiel::klondike {
     namespace {
@@ -36,16 +43,106 @@ namespace open_spiel::klondike {
         REGISTER_SPIEL_GAME(kGameType, Factory);
     }
 
+    // Card Methods ====================================================================================================
+
+    Card::Card(std::string rank, std::string suit, bool hidden) : rank(rank), suit(suit), hidden(hidden) {}
+
+    bool Card::operator==(Card other_card) {
+        return rank == other_card.rank and suit == other_card.suit;
+    }
+
+    // Deck Methods ====================================================================================================
+
+    Deck::Deck() {
+        for (auto suit : SUITS) {
+            for (auto rank : RANKS) {
+                Card current_card = Card {rank, suit, true};
+                cards.push_back(current_card);
+            }
+        }
+    }
+
+    void Deck::shuffle(int seed) {
+        if (!is_shuffled) {
+            std::shuffle(cards.begin(), cards.end(), std::default_random_engine(seed));
+            is_shuffled = true;
+            initial_order = cards;
+        } else {
+            std::cout << YELLOW << "WARNING: Deck is already shuffled" << RESET << std::endl;
+        }
+    }
+
+    void Deck::draw(int num_cards=3) {
+        std::deque<Card> dealt_cards = deal(num_cards);
+        for (Card & card : dealt_cards) {
+            card.hidden = false;
+        }
+        waste.insert(waste.begin(), dealt_cards.begin(), dealt_cards.end());
+    }
+
+    void Deck::rebuild() {
+        if (cards.empty()) {
+            for (Card card : initial_order) {
+                if (std::find(waste.begin(), waste.end(), card) != waste.end()) {
+                    cards.push_back(card);
+                }
+            }
+            waste.clear();
+            times_rebuilt += 1;
+        } else {
+            std::cout << YELLOW << "WARNING: Cannot rebuild a non-empty deck" << RESET << std::endl;
+        }
+    }
+
+    std::deque<Card> Deck::deal (unsigned long int num_cards) {
+        std::deque<Card> dealt_cards;
+        num_cards = std::min(num_cards, cards.size());
+        int i = 0;
+        while (i < num_cards) {
+            Card card = cards.front();
+            dealt_cards.push_back(card);
+            cards.pop_front();
+            i++;
+        }
+        return dealt_cards;
+    }
+
+    // Foundation Methods ==============================================================================================
+
+    Foundation::Foundation(char suit) : suit(suit) {};
+
+    // Tableau Methods =================================================================================================
+
+    Tableau::Tableau() {
+        cards = {};
+    }
+
+    Tableau::Tableau(std::deque<Card> provided_cards) {
+        cards = provided_cards;
+        for (Card & card : cards) {
+            card.hidden = true;
+        }
+        cards.back().hidden = false;
+    }
+
     // KlondikeState Methods ===========================================================================================
 
     KlondikeState::KlondikeState(std::shared_ptr<const Game> game) :
         State(game),
-        cur_player_(kChancePlayerId) {
+        cur_player_(kChancePlayerId),
+        is_setup_(false),
+        deck(),
+        foundations(),
+        tableaus() {
         // Empty
-    }
+    };
 
     Player                  KlondikeState::CurrentPlayer() const {
-        return 0;
+        if (!is_setup_) {
+            return kChancePlayerId;
+        } else {
+            return 0;
+        }
     }
 
     std::vector<Action>     KlondikeState::LegalActions() const {
@@ -56,7 +153,14 @@ namespace open_spiel::klondike {
         return std::string();
     };
 
-    std::string             KlondikeState::ToString() const {}
+    std::string             KlondikeState::ToString() const {
+        std::string result;
+        absl::StrAppend(&result, "Deck:");
+        for (Card card : deck.cards) {
+            absl::StrAppend(&result, card.rank, card.suit);
+        }
+
+    }
 
     bool                    KlondikeState::IsTerminal() const {}
 
@@ -102,4 +206,5 @@ namespace open_spiel::klondike {
     std::shared_ptr<const Game> KlondikeGame::Clone() const {
         return std::shared_ptr<const Game>(new KlondikeGame(*this));
     }
+
 }
--- open_spiel/games/klondike.h
+++ open_spiel/games/klondike.h
@@ -10,6 +10,51 @@
 namespace open_spiel::klondike {
     inline constexpr int kDefaultPlayers = 1;
 
+    const std::vector<std::string> SUITS = {"s", "h", "c", "d"};
+    const std::vector<std::string> RANKS = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"};
+
+    class Card {
+    public:
+        std::string rank;
+        std::string suit;
+        bool hidden;
+
+        Card(std::string rank, std::string suit, bool hidden);
+
+        bool operator==(Card other_card);
+    };
+
+    class Deck {
+    public:
+        std::deque<Card> cards;
+        std::deque<Card> waste;
+
+        Deck();
+        void shuffle(int seed);
+        void draw(int num_cards);
+        void rebuild();
+        std::deque<Card> deal(unsigned long int num_cards);
+
+    private:
+        int  times_rebuilt = 0;
+        bool is_shuffled = false;
+        std::deque<Card> initial_order;
+    };
+
+    class Foundation {
+    public:
+        char suit;
+        std::deque<Card> cards;
+        explicit Foundation(char suit);
+    };
+
+    class Tableau {
+    public:
+        std::deque<Card> cards;
+        Tableau();
+        explicit Tableau(std::deque<Card> provided_cards);
+    };
+
     class KlondikeGame;
 
     class KlondikeState : public State {
@@ -24,7 +69,11 @@ namespace open_spiel::klondike {
         std::unique_ptr<State>  Clone() const override;
 
     private:
-        int cur_player_;
+        int  cur_player_;
+        bool is_setup_;
+        Deck deck;
+        std::vector<Foundation> foundations;
+        std::vector<Tableau>    tableaus;
     };
 
     class KlondikeGame : public Game {
